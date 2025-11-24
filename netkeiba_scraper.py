498c8532c85f4a71f5d33ce88434a6f2fbf46210
vegaaaaaa01 <130959874+vegaaaaaa01@users.noreply.github.com>
2025-11-24 13:57:10 +0900

    Update netkeiba_scraper.py

--- netkeiba_scraper.py
+++ netkeiba_scraper.py
@@ -300,7 +300,7 @@ def export_one_book_all_venues_pretty_to_bytes(df: pd.DataFrame, zoom: int = 165
             ws.set_column(2, 2, 6)
             ws.set_column(3, 3, 20)
             ws.set_column(4, 4, 12)
-            ws.set_column(5, 5, 42)
+            ws.set_column(5, 5, 70)
             HEADER_ROW = 1
             DATA_START = 2
             n = len(out)
